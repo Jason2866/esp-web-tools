ad74af83c4e75b06cca34c7fef10659872c86611
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-04-08 14:42:25 +0000

    Github Action: Updated dist files

--- js/modules/install-dialog-FC6pNQ0r.js
+++ js/modules/install-dialog-FC6pNQ0r.js
@@ -1400,8 +1400,8 @@ import{e,i as t,_ as i,n as r,m as o,a as s,b as a,r as l,c as n,d,f as c,g as h
         cursor: pointer;
       }
       ew-dialog {
-        max-width: min(700px, 100% - 48px);
-        max-height: 80vh;
+        margin: 0 -24px;
+        padding: 0;
       }
       :host([state="LOGS"]) ew-dialog {
         max-width: 90vw;
