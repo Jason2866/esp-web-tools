9d64005c996d3521805651f2db6dde5cd8432fe1
Jason2866 <obermeier.johann@googlemail.com>
2026-05-02 17:52:04 +0200

    ensure timestamp has no color

--- src/util/timestamp-transformer.ts
+++ src/util/timestamp-transformer.ts
@@ -36,7 +36,7 @@ export class TimestampTransformer implements Transformer<string, string> {
     const h = date.getHours().toString().padStart(2, "0");
     const m = date.getMinutes().toString().padStart(2, "0");
     const s = date.getSeconds().toString().padStart(2, "0");
-    controller.enqueue(`[${h}:${m}:${s}] ${chunk}`);
+    controller.enqueue(`\x1b[0m[${h}:${m}:${s}] ${chunk}`);
   }
 
   reset() {
